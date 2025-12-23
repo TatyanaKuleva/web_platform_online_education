3cf095dc9d40329472541535a370cf2c86ecbce4
tatyana kuleva <tatianaarsentievna@gmail.com>
2025-12-23 21:10:49 +0300

    hw 4 task and additional task

--- materials/urls.py
+++ materials/urls.py
@@ -13,7 +13,7 @@ router.register("", CourseViewSet)
 urlpatterns = [
     path("lessons/", LessonListApiView.as_view(), name="lessons_list"),
     path("lessons/<int:pk>/", LessonRetrieveApiView.as_view(), name="lessons_retrieve"),
-    path("lessons/create/", LessonCreateApiView.as_view(), name="lessons_creste"),
+    path("lessons/create/", LessonCreateApiView.as_view(), name="lessons_create"),
     path("lessons/<int:pk>/delete/", LessonDestroyApiView.as_view(), name="lessons_delete"),
     path("lessons/<int:pk>/update/", LessonUpdateApiView.as_view(), name="lessons_update"),
 ]
--- users/serializers.py
+++ users/serializers.py
@@ -1,11 +1,25 @@
-from users.models import User
 from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 
-from rest_framework.serializers import ModelSerializer
+from users.models import Payment, User
+
+
+class PaymentSerializer(ModelSerializer):
+
+    class Meta:
+        model = Payment
+        fields = "__all__"
+
 
 class UserSerializer(ModelSerializer):
     password = serializers.CharField(write_only=True, required=False, allow_blank=True)
+    payments_of_user = SerializerMethodField()
+
+    def get_payments_of_user(self, obj):
+        payments = Payment.objects.filter(user=obj)
+        serializer = PaymentSerializer(payments, many=True)
+        return serializer.data
 
     class Meta:
         model = User
-        fields = '__all__'
+        fields = "__all__"
